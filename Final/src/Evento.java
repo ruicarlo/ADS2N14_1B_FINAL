c03a58f26ead355947bb7052eb633f029a46c07c
ruicarlo <ruicarlos@r8solucoes.com.br>
2014-06-19 12:51:17 -0300

    criação do projeto

---
+++ Final/src/Evento.java
@@ -0,0 +1,23 @@
+
+public class Evento {
+
+	
+	public String data;
+	public String comentario;
+	
+	
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	public String getComentario() {
+		return comentario;
+	}
+	public void setComentario(String comentario) {
+		this.comentario = comentario;
+	}
+	
+	
+}
---
+++ Final/src/Projeto.java
@@ -0,0 +1,22 @@
+
+public class Projeto {
+	
+	public String titulo;
+	public String descricao;
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+
+}
