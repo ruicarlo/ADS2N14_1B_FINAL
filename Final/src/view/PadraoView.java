a066037ed2098a5223b1852c43857b1479de6fab
ruicarlo <ruicarlos@r8solucoes.com.br>
2014-07-02 23:46:56 -0300

    criacao de metodos auxiliares para tabelas

--- Final/src/view/PadraoView.java
+++ Final/src/view/PadraoView.java
@@ -6,6 +6,7 @@ import static java.lang.System.out;
 import java.util.Scanner;
 
 import estruturas.Arquivos;
+import estruturas.Vector;
 
 public class PadraoView {
 	Scanner teclado;
@@ -34,5 +35,25 @@ public class PadraoView {
 	
 	public int lerInt() {
 		return teclado.nextInt();
+	}
+	private boolean verificaSePosicaoUsada(int posicao, int[]posicoes) {
+		for(int i=0; i<posicoes.length;i++) {
+			if(posicao==posicoes[i]) {
+				return true;
+			}
+		}
+		return false;
+	}
+	public int verificarMaiorTexto(Vector<String> tabela, int[] posicoes) {
+		int maior = 0;
+		for(String item : tabela.asArray()) {
+			String[] registro = arquivo.explodirLinhaDoArquivo(item);
+			for(int i=0; i<registro.length;i++ ) {
+				if(registro[i].length() > maior && verificaSePosicaoUsada(i, posicoes)) {
+					maior = registro[i].length();
+				}
+			}
+		}
+		return maior;
 	}
 }
